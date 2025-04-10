81b62ddf9f3b6bfb3e896023903636e119e0d07b
Kai-Doh <github@kaidoh.ch>
2025-04-10 18:23:10 +0200

    modification of prompt

--- minishell.c
+++ minishell.c
@@ -6,27 +6,52 @@
 /*   By: ktiomico <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 10:42:20 by ktiomico          #+#    #+#             */
-/*   Updated: 2025/04/10 17:14:45 by ktiomico         ###   ########.fr       */
+/*   Updated: 2025/04/10 18:15:04 by ktiomico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **env)
+int	handle_input(char *rl)
+{
+	if (ft_strncmp(rl, "exit", 4) == 0)
+		return (1);
+	printf(PURPLE "42\n" RESET);
+	return (0);
+}
+
+void	start_shell_loop(char **env)
 {
 	char	*rl;
+	char	*prompt_str;
+
+	(void)env;
+	while (1)
+	{
+		prompt_str = prompt();
+		if (!prompt_str)
+			continue ;
+		rl = readline(prompt_str);
+		free(prompt_str);
+		if (!rl)
+			break ;
+		if (handle_input(rl))
+		{
+			free(rl);
+			break ;
+		}
+		free(rl);
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
 	t_data	data;
 
 	(void)argv;
-	(void)env;
 	(void)data;
 	if (argc != 1)
 		exit_msg(ARGS, ERROR);
-	while (1)
-	{
-		rl = readline(GREEN "👾 Minishell ➜ " RESET);
-		if (ft_strncmp(rl, "exit", 4) == 0)
-			return (0);
-		printf("42\n");
-	}
+	start_shell_loop(env);
+	return (0);
 }
